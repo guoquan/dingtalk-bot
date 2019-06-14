f82fb17ae48c59105c30bb01c868b0e07195c34d
Guo Quan <guoquanscu@gmail.com>
2019-06-14 01:19:45 -0500

    Update import message by package name

--- dtb/bot/base.py
+++ dtb/bot/base.py
@@ -1,6 +1,6 @@
 from urllib import request
 from collections import deque
-from ..message import Message
+from .. import message
 import time
 
 
@@ -71,7 +71,7 @@ class Bot(object):
     @staticmethod
     def register(name):
         def register_message(cls_):
-            if not issubclass(cls_, Message):
+            if not issubclass(cls_, message.Message):
                 raise TypeError('Register Message class with Bot, while {} is provided.'.format(cls_))
             self.message_types[name] = cls_
         return register_message
